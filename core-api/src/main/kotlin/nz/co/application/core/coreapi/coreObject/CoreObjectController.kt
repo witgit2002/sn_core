a0661ad518161e71224afdf2d65e129ccfcba5c7
va1477 <wit2002@gmail.com>
2017-11-02 18:03:18 +1300

    swagger ui is added

--- core-api/src/main/kotlin/nz/co/application/core/coreapi/coreObject/CoreObjectController.kt
+++ core-api/src/main/kotlin/nz/co/application/core/coreapi/coreObject/CoreObjectController.kt
@@ -1,10 +1,10 @@
 package nz.co.application.core.coreapi.coreObject
 
+import io.swagger.annotations.ApiOperation
 import nz.co.application.core.coreapi.coreParameter.CoreParameter
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
@@ -34,6 +34,7 @@ class CoreObjectController
 		return changesInLog.toString()
 	}
 
+	@ApiOperation(value = "get object by id", notes = "get object by id", tags = arrayOf("Object Management"))
 	@GetMapping("/object")
 	fun getObjectById(@RequestParam(name = "id") id: Long): CoreObjectEntity
 	{
--- core-api/src/main/kotlin/nz/co/application/core/coreapi/objectType/ObjectTypeController.kt
+++ core-api/src/main/kotlin/nz/co/application/core/coreapi/objectType/ObjectTypeController.kt
@@ -1,3 +1,16 @@
 package nz.co.application.core.coreapi.objectType
 
+import io.swagger.annotations.ApiOperation
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class ObjectTypeController
+{
+	@ApiOperation(value = "get object type by id", notes = "get object type by id", tags = arrayOf("Object Type Management"))
+	@GetMapping("/objectType")
+	fun getObjectTypeById(id: Long): ObjectTypeEntity
+	{
+		return ObjectTypeEntity(1, "test name", "test description")
+	}
+}
