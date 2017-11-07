4eea9c9ae818da97e14fb1aca03579ff0f3f16f9
va1477 <wit2002@gmail.com>
2017-11-07 16:20:55 +1300

    id generator is working

--- core-api/src/main/kotlin/nz/co/application/core/coreapi/idGenerator/IdGeneratorController.kt
+++ core-api/src/main/kotlin/nz/co/application/core/coreapi/idGenerator/IdGeneratorController.kt
@@ -1,6 +1,29 @@
 package nz.co.application.core.coreapi.idGenerator
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class IdGeneratorController
 {
+	@Autowired
+	lateinit var idGeneratorDAO: IdGeneratorDAO
+
+	val DEFAULT_RANGE_SIZE = 100
+	private var currentValue: Long = 0
+	private var maxValue: Long = 0
+
+	@GetMapping("/getNextId")
+	fun getNextId(): Long
+	{
+		if (currentValue >= maxValue)
+		{
+			maxValue = idGeneratorDAO.loadNewRange(rangeSize = DEFAULT_RANGE_SIZE)
+			currentValue = maxValue - DEFAULT_RANGE_SIZE
+		}
+
+		return currentValue++
+	}
 
 }
